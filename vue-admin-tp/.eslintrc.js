d6d1ab6f32151198d8cc543cc67afc0990acd6ef
liyahao <1151050603@qq.com>
2021-05-20 10:23:38 +0800

    修复模板

--- vue-admin-tp/.eslintrc.js
+++ vue-admin-tp/.eslintrc.js
@@ -15,6 +15,14 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // vue html的eslint工具
+    "vue/html-indent": ["error", 2, {
+      "attribute": 1,
+      "baseIndent": 1,
+      "closeBracket": 0,
+      "alignAttributesVertically": true,
+      "ignores": []
+    }]
   }
 }
